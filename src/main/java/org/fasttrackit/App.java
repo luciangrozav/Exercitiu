c6f8630aad94b90746ed8ab417930417cee2258a
luciangrozav <luciangrozav01@gmail.com>
2021-02-13 12:02:24 +0200

    Citire parametrii, realizare de operatii.

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,16 @@
+package org.fasttrackit;
+
+
+public class App 
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Exercitiu de weekend!" );
+        Operation operation = new Operation();
+        operation.suma();
+    }
+
+
+
+}
