27ce970c199430e94f92c99c8fcf7e1cceca4571
luciangrozav <luciangrozav01@gmail.com>
2021-03-05 13:11:25 +0200

    Logical operations;

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,13 +6,16 @@ public class App
     public static void main( String[] args )
 
     {
-        System.out.println( "Overloading" );
+//        System.out.println( "Overloading" );
 
         Operation operation = new Operation();
 
-        System.out.println(operation.suma(2));
-        System.out.println(operation.inmultire(55, 10));
-        System.out.println(operation.impartire(10, 2));
+        operation.logica();
+        operation.stea();
+
+//        System.out.println(operation.suma(2));
+//        System.out.println(operation.inmultire(55, 10));
+//        System.out.println(operation.impartire(10, 2));
 
     }
 
--- src/main/java/org/fasttrackit/Operation.java
+++ src/main/java/org/fasttrackit/Operation.java
@@ -1,27 +1,60 @@
 package org.fasttrackit;
 
 
+import org.fasttrackit.utils.ScannerUtils;
+
 public class Operation {
 
-public double suma (double a)  // overloading la metoda suma
-{
-    return suma(a, 10);
-}
+int nr1, sum=0;
+    public void logica () {
 
-public double suma ( double a, double b)
-{
-    return a+b;
-}
 
-public double inmultire (double a, double b)
-{
-    return a*b;
-}
+        System.out.println("Numar= ");
+        nr1 = ScannerUtils.nextIntAndMoveToNextLine();
+        for (int i = nr1; i <= 100 ; i++)
+            sum += i;
+        System.out.println("Suma= " + sum);
+        System.out.println("Media= " + sum/(100-nr1));
+    }
+    public void stea() {
+        int i, j, nr = 50;
+        for (i = 0; i < nr; i++) {
+            System.out.println(" ");
+        for(j=0; j< nr -i; j++)
+            System.out.print("*");
+        }
+    }
+//
+//        System.out.println("Numar2= ");
+//        nr2 = ScannerUtils.nextIntAndMoveToNextLine();
+//        if(nr1<nr2)
+//        {for (int i = nr1; i <= nr2; i++)
+//            System.out.println(i);}
+//        if (nr2<nr1)
+//        {for (int i = nr2; i <= nr1; i++)
+//            System.out.println(i);}
+//        if (nr1==nr2)
+//            System.out.println(nr1);
 
-public double impartire (double a, double b)
-{
-    return a/b;
-}
+//public double suma (double a)  // overloading la metoda suma
+//{
+//    return suma(a, 10);
+//}
+//
+//public double suma ( double a, double b)
+//{
+//    return a+b;
+//}
+//
+//public double inmultire (double a, double b)
+//{
+//    return a*b;
+//}
+//
+//public double impartire (double a, double b)
+//{
+//    return a/b;
+//}
 
 
 //private int geta ()  // pt citirea variabilelor
